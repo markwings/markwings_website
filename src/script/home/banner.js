eef3da48f6670a3a5de440a9ea7e701ebe5cd97a
Harshit Tiwari <harshit54646587@gmail.com>
2025-07-13 11:55:01 +0530

    Banner section bugs fixed

--- src/script/home/banner.js
+++ src/script/home/banner.js
@@ -7,7 +7,6 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // Creating a scene
 const scene = new THREE.Scene();
 const canvas = document.getElementById("3dCanvas");
-const canvas2 = document.getElementById("3dCanvas2");
 
 // Initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -29,7 +28,6 @@ renderer.setSize(window.innerWidth/1.75, window.innerHeight/1.75);
 
 // Load the model
 let logo;
-let logo2;
 const loader = new GLTFLoader();
 const scalingFactor = Math.min(Math.max(window.innerWidth / 1000, 0.5), 1);
 loader.load("/logo.glb", function (model) {
@@ -39,14 +37,6 @@ loader.load("/logo.glb", function (model) {
   logo.rotation.set(1.55, -0.01, -0.65);
   logo.matrixWorldNeedsUpdate = true;
   scene.add(logo);
-});
-
-loader.load("/logo.glb", function (model) {
-  logo2 = model.scene;
-  logo2.scale.setScalar(3 * scalingFactor);
-  logo2.rotation.set(1.55, -0.01, -0.65);
-  logo2.matrixWorldNeedsUpdate = true;
-  scene.add(logo2);
 });
 
 // Handle window resize
