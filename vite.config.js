0a88a1e822f9f22609155b8f5e64b1b923165424
Abhay Dave <adlove03012004@gmail.com>
2025-10-12 13:40:15 +0530

    Build error fixes

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        marketing: path.resolve(__dirname, "services/marketing.html"),
+        development: path.resolve(__dirname, "services/development.html"),
+        graphics: path.resolve(__dirname, "services/graphics.html"),
+      },
+    },
+  },
+});
