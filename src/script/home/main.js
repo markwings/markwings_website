c8121bb8fedbca55d71da0daa4cd67a64b50628d
Harshit Tiwari <harshit54646587@gmail.com>
2025-10-16 19:17:55 +0530

    Bug fixing for planToAct for tablet and mobile devices.

--- src/script/home/main.js
+++ src/script/home/main.js
@@ -3,26 +3,28 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function dateTime(){
+function dateTime() {
   // Date and Time Display
   let dateTimeContainer = document.getElementById("dateTime");
   let dateTimeContainer2 = document.getElementById("dateTime2");
-  
-  dateTimeContainer.innerHTML = new Date().toLocaleString().replace(/,/g, "<br>");
+
+  dateTimeContainer.innerHTML = new Date()
+    .toLocaleString()
+    .replace(/,/g, "<br>");
   setInterval(() => {
     dateTimeContainer.innerHTML = new Date()
       .toLocaleString()
       .replace(/,/g, "<br>");
   }, 1000);
 
+  dateTimeContainer2.innerHTML = new Date()
+    .toLocaleString()
+    .replace(/,/g, "<br>");
+  setInterval(() => {
     dateTimeContainer2.innerHTML = new Date()
       .toLocaleString()
       .replace(/,/g, "<br>");
-    setInterval(() => {
-      dateTimeContainer2.innerHTML = new Date()
-        .toLocaleString()
-        .replace(/,/g, "<br>");
-    }, 1000);
+  }, 1000);
 }
 dateTime();
 
@@ -31,24 +33,23 @@ dateTime();
 //   document.querySelector(".panelCon").style.bottom = window.scrollY * -1 + "px";
 // });
 
-
-document.querySelectorAll(".fadeIn").forEach((item)=>{
-  gsap.from(item,{
+document.querySelectorAll(".fadeIn").forEach((item) => {
+  gsap.from(item, {
     y: 30,
     opacity: 0,
     duration: 1,
-    scrollTrigger:{
+    scrollTrigger: {
       trigger: item,
       scroller: "body",
       start: "top 90%",
       end: "top 75%",
-      scrub: true
-    }
+      scrub: true,
+    },
   });
 });
 
-document.querySelectorAll(".threeD-element").forEach((item)=>{
-  function threeDAnimation(x,y){
+document.querySelectorAll(".threeD-element").forEach((item) => {
+  function threeDAnimation(x, y) {
     let positionPx = x - item.getBoundingClientRect().left;
     let positionX = (50 - (positionPx / item.offsetWidth) * 100) / 3;
     let positionPy = y - item.getBoundingClientRect().top;
@@ -57,35 +58,42 @@ document.querySelectorAll(".threeD-element").forEach((item)=>{
     item.style.setProperty("--rX", positionX + "deg");
     item.style.setProperty("--rY", positionY + "deg");
   }
-  item.addEventListener("mousemove",(e)=>{
+  item.addEventListener("mousemove", (e) => {
     threeDAnimation(e.x, e.y);
   });
-  item.addEventListener("touchstart",(e)=>{
-    document.getElementById("intro-bg-video").style.opacity = "1";
+  item.addEventListener("mouseout", () => {
+    item.style.setProperty("--rX", "0deg");
+    item.style.setProperty("--rY", "0deg");
   });
-  item.addEventListener("touchend",()=>{
-    document.getElementById("intro-bg-video").style.opacity = "0";
-  })
-  item.addEventListener("mouseout",()=>{
+
+  item.addEventListener("touchend", () => {
     item.style.setProperty("--rX", "0deg");
     item.style.setProperty("--rY", "0deg");
   });
 });
 
-function strings(){
+document.querySelectorAll(".threeD-video-element").forEach((item) => {
+  item.addEventListener("touchstart", (e) => {
+    document.getElementById("intro-bg-video").style.opacity = "1";
+  });
+  item.addEventListener("touchend", () => {
+    document.getElementById("intro-bg-video").style.opacity = "0";
+  });
+});
+
+function strings() {
   let initialPath = "M 10 200 Q 700 200 1390 200";
   let finalPath = "M 10 200 Q 700 200 1390 200";
   let el = document.getElementById("string");
-  el.addEventListener("mousemove",e=>{
-    initialPath = `M 10 200 Q ${e.x} ${e.y-200} 1390 200`;
-    gsap.set("#string path",{
+  el.addEventListener("mousemove", (e) => {
+    initialPath = `M 10 200 Q ${e.x} ${e.y - 200} 1390 200`;
+    gsap.set("#string path", {
       attr: {
-        d: initialPath
-      }
-    })
-    
-  })
-  el.addEventListener("mouseleave",()=>{
+        d: initialPath,
+      },
+    });
+  });
+  el.addEventListener("mouseleave", () => {
     gsap.to("#string path", {
       attr: {
         d: finalPath,
@@ -93,6 +101,6 @@ function strings(){
       duration: 1,
       ease: "elastic.out(1,0.2)",
     });
-  })
+  });
 }
 strings();
